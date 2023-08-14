01e2d35fcda522ee634bcd459d4057b8f71f9f62
Tim <timfill2012@gmail.com>
2023-08-14 07:41:31 +0300

    test: "Int и Integer"

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer ins = 2;
+        print(1);
+        print(ins);
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int i) {
+        
+    }
+    public static void print(Integer in){
+        
+    }
+}
