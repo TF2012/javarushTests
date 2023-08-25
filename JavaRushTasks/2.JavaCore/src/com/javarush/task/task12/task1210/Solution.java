aa2e597b75e7d3a453fc10c24351d8c708e76c34
Tim <timfill2012@gmail.com>
2023-08-25 19:37:10 +0300

    test: "Три метода и максимум "

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    
+    public static int max(int i, int n) {
+        return i > n ? i : n;
+    }
+    public static long max(long l, long o) {
+        return l > o ? l : o;
+    }
+    public static double max(double d, double ou) {
+        return d > ou ? d : ou;
+    }
+}
