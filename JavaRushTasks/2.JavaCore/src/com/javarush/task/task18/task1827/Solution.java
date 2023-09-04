ae1afc88ecd7ccdd82722deec9d06901d3ec8c74
Tim <timfill2012@gmail.com>
2023-09-04 21:55:37 +0300

    test: "Прайсы" начало

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task18.task1827;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Прайсы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            String file1 = bufferedReader.readLine();
+            String file2 = bufferedReader.readLine();
+            FileReader fileReader = new FileReader()
+        }
+    }
+}
